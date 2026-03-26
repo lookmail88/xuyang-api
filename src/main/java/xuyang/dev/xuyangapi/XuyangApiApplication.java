ff047c4907503f9c1ecde20369cdb3b7c940d089
xgao <xuyang.gao@medimpact.com>
2026-03-26 10:43:42 -0700

    Initial commit

---
+++ src/main/java/xuyang/dev/xuyangapi/XuyangApiApplication.java
@@ -0,0 +1,13 @@
+package xuyang.dev.xuyangapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuyangApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuyangApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xuyang/dev/xuyangapi/XuyangApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xuyang.dev.xuyangapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuyangApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
