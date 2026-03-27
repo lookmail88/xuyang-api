e92ef85196db1f42ebb33abcc5cddc13dde66e2a
xgao <xuyang.gao@medimpact.com>
2026-03-26 17:53:36 -0700

    initial check in

---
+++ src/main/java/xuyang/dev/xuyangapi/web/WebController.java
@@ -0,0 +1,14 @@
+package xuyang.dev.xuyangapi.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping(value="/sayhello")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello,my friends");
+    }
+}
