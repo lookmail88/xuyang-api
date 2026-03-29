60c10f4afd66adc4a4992acd14059239bde3751a
xgao <xuyang.gao@medimpact.com>
2026-03-28 17:33:31 -0700

    fix: add java code for swagger UI config

---
+++ src/main/java/xuyang/dev/xuyangapi/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package xuyang.dev.xuyangapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("https://api-dev.xuyang.dev/xuyang-api")
+                                .description("production gateway")
+
+                ));
+
+    }
+
+}
+
