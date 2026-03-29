254d4dd273d27c708273ba551ccddb33a2d56270
xgao <xuyang.gao@medimpact.com>
2026-03-29 14:22:29 -0700

    fix: add timestamp for UI to check back-end status

--- src/main/java/xuyang/dev/xuyangapi/web/WebController.java
+++ src/main/java/xuyang/dev/xuyangapi/web/WebController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @CrossOrigin
 public class WebController {
@@ -13,4 +16,12 @@ public class WebController {
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hello,This is for my Argo");
     }
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss_SSS");
+
+    @GetMapping(value="/health")
+    public ResponseEntity<String> getHealth(){
+        LocalDateTime now = LocalDateTime.now();
+        return ResponseEntity.ok( now.format(formatter));
+    }
+
 }
