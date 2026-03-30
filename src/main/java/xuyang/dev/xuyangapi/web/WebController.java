40312e008465c4ec289e9768ddacc8bf9e1c3511
xgao <xuyang.gao@medimpact.com>
2026-03-29 17:48:23 -0700

    fix: update controller

--- src/main/java/xuyang/dev/xuyangapi/web/WebController.java
+++ src/main/java/xuyang/dev/xuyangapi/web/WebController.java
@@ -25,7 +25,7 @@ public class WebController {
 
     @GetMapping(value="/sayhello")
     public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hello,This is for start");
+        return ResponseEntity.ok("Hello,This is for start2");
     }
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss_SSS");
 
